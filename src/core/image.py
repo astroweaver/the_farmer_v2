5ffb7ba0cf48069f28f2ba6c1404ba9939548414
astroweaver <john.weaver.astro@gmail.com>
2023-05-10 19:15:20 -0400

    fix

--- src/core/image.py
+++ src/core/image.py
@@ -1277,7 +1277,7 @@ class BaseImage():
             outname = os.path.join(conf.PATH_FIGURES, fnsrc + self.filename.replace('.h5', '_summary.pdf'))
             pdf = matplotlib.backends.backend_pdf.PdfPages(outname)
 
-            for band in self.model_tracker[source_id][stage].keys():
+            for band in self.model_tracker[source_id][stage] if band in conf.BANDS:
 
                 bandname = 'detection'
                 if band != 'detection':
